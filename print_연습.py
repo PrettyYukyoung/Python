8a6bbcfc81ce57d40280d592e99fefabc11fc6e4
귀염뽀짝유경이 <65803951+PrettyYukyoung@users.noreply.github.com>
2021-04-08 13:07:10 +0900

    Create print_연습.py

---
+++ print_연습.py
@@ -0,0 +1,10 @@
+# 애완동물을 소개해주세요~
+name = "땅콩"
+animal = "고양이"
+age = 5
+hobby = "간식먹기"
+is_adult = age >= 3 
+
+print("우리집 "+animal+"의 이름은 "+ name+"이에요")
+print(name,"는 ",age,"살이며 "+hobby+"를 아주 좋아해요")
+print(name+"는 어른일까요?"+str(is_adult))
